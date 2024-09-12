be889aee1d79eb4781bc92e5c327e7876f99270f
insanise <alp.yeni@metu.edu.tr>
2024-09-13 01:19:39 +0300

    Dashboard Deneme

--- src/routes/_layout/index.tsx
+++ src/routes/_layout/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Container, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
+import { LineChart, Line, PieChart, Pie } from "recharts"; // Import the necessary components
 
 import useAuth from "../../hooks/useAuth";
 
@@ -9,7 +10,58 @@ export const Route = createFileRoute("/_layout/")({
 
 function Dashboard() {
   const { user: currentUser } = useAuth();
-
+  const data01 = [
+    {
+      name: "Group A",
+      value: 400,
+    },
+    {
+      name: "Group B",
+      value: 300,
+    },
+    {
+      name: "Group C",
+      value: 300,
+    },
+    {
+      name: "Group D",
+      value: 200,
+    },
+    {
+      name: "Group E",
+      value: 278,
+    },
+    {
+      name: "Group F",
+      value: 189,
+    },
+  ];
+  const data02 = [
+    {
+      name: "Group A",
+      value: 2400,
+    },
+    {
+      name: "Group B",
+      value: 4567,
+    },
+    {
+      name: "Group C",
+      value: 1398,
+    },
+    {
+      name: "Group D",
+      value: 9800,
+    },
+    {
+      name: "Group E",
+      value: 3908,
+    },
+    {
+      name: "Group F",
+      value: 4800,
+    },
+  ];
   return (
     <>
       <Container maxW="full">
@@ -19,7 +71,30 @@ function Dashboard() {
           </Text>
           <Text>Welcome back, nice to see you again!</Text>
           <SimpleGrid marginTop={"10px"} columns={4} spacing={10}>
-            <Box bg="#50505020" borderRadius={"10px"} height="170px"></Box>
+            <Box bg="#50505020" borderRadius={"10px"} height="170px">
+              <PieChart width={200} height={200}>
+                <Pie
+                  data={data01}
+                  dataKey="value"
+                  nameKey="name"
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={50}
+                  fill="#8884d8"
+                />
+                <Pie
+                  data={data02}
+                  dataKey="value"
+                  nameKey="name"
+                  cx="50%"
+                  cy="50%"
+                  innerRadius={60}
+                  outerRadius={80}
+                  fill="#82ca9d"
+                  label
+                />
+              </PieChart>
+            </Box>
             <Box bg="#50505020" borderRadius={"10px"} height="170px"></Box>
             <Box bg="#50505020" borderRadius={"10px"} height="170px"></Box>
             <Box bg="#50505020" borderRadius={"10px"} height="170px"></Box>
